99a652380c55514bc9ec0b5d9c6cd75122c81862
Thinus Barnard <thinusplanb97@gmail.com>
2019-07-31 09:55:06 +0200

    Apply fixes from StyleCI (#1)

--- src/JokeFactory.php
+++ src/JokeFactory.php
@@ -5,19 +5,18 @@ namespace Mcbarnard\ChuckNorrisJokes;
 class JokeFactory
 {
     protected $jokes = [
-        "Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.",
+        'Chuck Norris does not wear a condom. Because there is no such thing as protection from Chuck Norris.',
         "Chuck Norris' tears cure cancer. Too bad he has never cried.",
-        "Chuck Norris counted to infinity... Twice.",
+        'Chuck Norris counted to infinity... Twice.',
         "If you can see Chuck Norris, he can see you. If you can't see Chuck Norris you may be only seconds away from death.",
-        "There is no theory of evolution. Just a list of animals Chuck Norris allows to live."
+        'There is no theory of evolution. Just a list of animals Chuck Norris allows to live.',
     ];
 
-    public function __construct(array $jokes=null)
+    public function __construct(array $jokes = null)
     {
-        if ($jokes) 
-        {
+        if ($jokes) {
             $this->jokes = $jokes;
-        }   
+        }
     }
 
     public function getRandomJoke()
@@ -25,5 +24,3 @@ class JokeFactory
         return $this->jokes[array_rand($this->jokes)];
     }
 }
-
-?>
