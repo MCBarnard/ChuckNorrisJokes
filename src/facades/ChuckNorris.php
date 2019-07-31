0f9715bdaee8163ea02c85cca67deb264be0c6a8
Thinus Barnard <thinusplanb97@gmail.com>
2019-07-31 16:33:25 +0200

    Create ChuckNorris.php

---
+++ src/facades/ChuckNorris.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace Mcbarnard\ChuckNorrisJokes\Facades;
+
+use Illuminate\Support\Facades\Facade; 
+
+class ChuckNorris extends Facade
+{
+    protected static function getFacadeAccessor()
+    {
+        return 'chuck-norris';
+    }
+}
