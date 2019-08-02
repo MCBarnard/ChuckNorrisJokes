a203e49bcc179d4828c31a02c68325e8d047381c
Thinus Barnard <thinusplanb97@gmail.com>
2019-08-02 14:03:37 +0200

    Create ChuckNorrisJoke.php

---
+++ src/commands/ChuckNorrisJoke.php
@@ -0,0 +1,18 @@
+<?php
+
+namespace Mcbarnard\ChuckNorrisJokes\Console;
+
+use Illuminate\Console\Command;
+use Mcbarnard\ChuckNorrisJokes\Facades\ChuckNorris;
+
+class ChuckNorrisJoke extends Command
+{
+    protected $signature = 'chuck-norris';
+  
+    protected $description = 'Output a funny Chuck Norris joke';
+
+    public function handle()
+    {
+        $this->info(ChuckNorris::getRandomJoke());
+    }
+}
